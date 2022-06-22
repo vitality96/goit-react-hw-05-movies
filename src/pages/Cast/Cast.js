0d12698d0bb9a8050e2e247bb8fdc0d07d9f4bac
Vitalii Peresoliak <vitalik21022@gmail.com>
2022-06-22 20:10:22 +0300

    change image scr

--- src/pages/Cast/Cast.js
+++ src/pages/Cast/Cast.js
@@ -21,7 +21,7 @@ export default function Cast() {
                             {profile_path ? (
                                 (<img src={`https://image.tmdb.org/t/p/w200${profile_path}`}
                                     alt={name} />)
-                            ) : (<img src="https://bitsofco.de/content/images/2018/12/Screenshot-2018-12-16-at-21.06.29.png"
+                            ) : (<img src="https://i5.walmartimages.com/asr/f752abb3-1b49-4f99-b68a-7c4d77b45b40_1.39d6c524f6033c7c58bd073db1b99786.jpeg"
                                 alt={name} width="200" height="300" />)}
                             <p className={s.text}>{name}</p>
                             <p className={s.subtitle}>{character}</p>
